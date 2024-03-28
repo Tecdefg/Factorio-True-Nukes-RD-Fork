7c04cd14f39e2f0a22374d66d02c6f23d60dcaa9
Tecdefg <88845361+Tecdefg@users.noreply.github.com>
2024-03-28 19:22:39 +0100

    Update data-nukes-warheads-weapon-appearance.lua

--- True-Nukes_0.3.33/prototypes/nukes/data-nukes-warheads-weapon-appearance.lua
+++ True-Nukes_0.3.33/prototypes/nukes/data-nukes-warheads-weapon-appearance.lua
@@ -6,14 +6,9 @@ local tints = create_utils.tints
 
 if weaponTypes["rounds-magazine"] then
   weaponTypes["rounds-magazine"].icons["-atomic-0_1t"] = { "__True-Nukes__/graphics/rounds/yy-atomic-rounds-magazine.png"}
-  weaponTypes["rounds-magazine"].icons["-atomic-0_5t"] = { "__True-Nukes__/graphics/rounds/yg-atomic-rounds-magazine.png"}
-  weaponTypes["rounds-magazine"].icons["-atomic-2t"] = { "__True-Nukes__/graphics/rounds/gg-atomic-rounds-magazine.png"}
 end
 if weaponTypes["Schall-sniper-magazine"] then
   weaponTypes["Schall-sniper-magazine"].icons["-atomic-0_1t"] = { "__True-Nukes__/graphics/rounds/yy-atomic-rounds-magazine.png"}
-  weaponTypes["Schall-sniper-magazine"].icons["-atomic-0_5t"] = { "__True-Nukes__/graphics/rounds/yy-atomic-rounds-magazine.png"}
-  weaponTypes["Schall-sniper-magazine"].icons["-atomic-2t"] = { "__True-Nukes__/graphics/rounds/yg-atomic-rounds-magazine.png"}
-  weaponTypes["Schall-sniper-magazine"].icons["-atomic-4t"] = { "__True-Nukes__/graphics/rounds/gg-atomic-rounds-magazine.png"}
 end
 
 local setupForShotgun = {
@@ -22,11 +17,6 @@ local setupForShotgun = {
   warheads = {}
 }
 setupForShotgun.warheads["-atomic-0_1t"] = {style = 1, tints = {tints.californium}}
-setupForShotgun.warheads["-atomic-0_5t"] = {style = 2, tints = {tints.californium, tints.nothing}}
-setupForShotgun.warheads["-atomic-2t"] = {style = 2, tints = {tints.californium, tints.californium}}
-setupForShotgun.warheads["-atomic-4t"] = {style = 2, tints = {tints.californium, tints.uraniumLive}}
-setupForShotgun.warheads["-atomic-8t"] = {style = 2, tints = {tints.uraniumLive, tints.californium}}
-setupForShotgun.warheads["-atomic-20t"] = {style = 2, tints = {tints.uraniumLive, tints.uraniumLive}}
 
 setupWarheadsForWeapon(setupForShotgun)
 
@@ -36,8 +26,6 @@ local setupForShotgunBuck = {
   warheads = {}
 }
 setupForShotgunBuck.warheads["-atomic-0_1t"] = {style = 3, tints = {tints.nothing, tints.californium}}
-setupForShotgunBuck.warheads["-atomic-0_5t"] = {style = 3, tints = {tints.californium, tints.californium}}
-setupForShotgunBuck.warheads["-atomic-2t"] = {style = 3, tints = {tints.californium, tints.uraniumLive}}
 
 setupWarheadsForWeapon(setupForShotgunBuck)
 
@@ -47,14 +35,8 @@ local setupForCannon = {
   weapon = "cannon-shell",
   warheads = {}
 }
-setupForCannon.warheads["-atomic-0_1t"] = {style = 3, tints = {tints.californium}}
 setupForCannon.warheads["-atomic-0_5t"] = {style = 3, tints = {tints.californium}}
-setupForCannon.warheads["-atomic-2t"] = {style = 2, tints = {tints.californium}}
-setupForCannon.warheads["-atomic-4t"] = {style = 4, tints = {tints.californium, tints.californium}}
-setupForCannon.warheads["-atomic-8t"] = {style = 4, tints = {tints.californium, tints.uraniumLive}}
-setupForCannon.warheads["-atomic-20t"] = {style = 4, tints = {tints.uraniumLive, tints.californium}}
 
-setupForCannon.warheads["-atomic-500t"] = {style = 4, tints = {tints.uraniumLive, tints.uraniumLive}}
 setupWarheadsForWeapon(setupForCannon)
 
 if mods["SchallTankPlatoon"] then
@@ -70,13 +52,8 @@ local setupForRocket = {
   weapon = "small-rocket",
   warheads = {}
 }
-setupForRocket.warheads["-atomic-0_1t"] = {style = 2, tints = {tints.californium}}
-setupForRocket.warheads["-atomic-0_5t"] = {style = 3, tints = {tints.lightNothing, tints.californium}}
 setupForRocket.warheads["-atomic-2t"] = {style = 3, tints = {tints.californium, tints.californium}}
 
-setupForRocket.warheads["-atomic-4t"] = {style = 4, tints = {tints.lightNothing, tints.californium, tints.californium}}
-setupForRocket.warheads["-atomic-8t"] = {style = 4, tints = {tints.californium, tints.californium, tints.californium}}
-setupForRocket.warheads["-atomic-20t"] = {style = 5, tints = {tints.californium, tints.californium, tints.californium, tints.californium}}
 setupWarheadsForWeapon(setupForRocket)
 
 local setupForBigRocket = {
@@ -84,16 +61,9 @@ local setupForBigRocket = {
   weapon = "big-rocket",
   warheads = {}
 }
-setupForBigRocket.warheads["-atomic-0_1t"] = {style = 2, tints = {tints.californium}}
-setupForBigRocket.warheads["-atomic-0_5t"] = {style = 3, tints = {tints.nothing, tints.californium}}
-setupForBigRocket.warheads["-atomic-2t"] = {style = 3, tints = {tints.californium, tints.californium}}
 
 setupForBigRocket.warheads["-atomic-4t"] = {style = 2, tints = {tints.uraniumLive}}
-setupForBigRocket.warheads["-atomic-8t"] = {style = 3, tints = {tints.nothing, tints.uraniumLive}}
-setupForBigRocket.warheads["-atomic-20t"] = {style = 3, tints = {tints.uraniumLive, tints.uraniumLive}}
 
-setupForBigRocket.warheads["-atomic-500t"] = {style = 4, tints = {tints.uraniumLive, tints.uraniumLive, tints.uraniumLive}}
-setupForBigRocket.warheads["-atomic-1kt"] = {style = 5, tints = {tints.uraniumLive, tints.uraniumLive, tints.uraniumLive, tints.uraniumLive}}
 setupWarheadsForWeapon(setupForBigRocket)
 
 
@@ -115,6 +85,10 @@ setupForArtillery.warheads["-atomic-15kt"] = {style = 5, tints = {tints.uraniumL
 setupForArtillery.warheads["-atomic-2-stage-15kt"] = {style = 5, tints = {tints.uraniumLive, tints.nothing}}
 
 setupForArtillery.warheads["-atomic-2-stage-100kt"] = {style = 5, tints = {tints.uraniumLive, tints.tritium}}
+setupForArtillery.warheads["-1Mt"] = {style = 5, tints = {tints.uraniumLive, tints.tritium}}
+setupForArtillery.warheads["-5Mt"] = {style = 5, tints = {tints.uraniumLive, tints.tritium}}
+setupForArtillery.warheads["-10Mt"] = {style = 5, tints = {tints.uraniumLive, tints.tritium}}
+
 setupWarheadsForWeapon(setupForArtillery)
 
 
